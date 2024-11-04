c2a7643fb97e886a8f9d4b9175cafb26d179eb69
Matheus Pimentel <matheus08.pimentel@gmail.com>
2024-11-03 21:51:38 -0300

    chore: created colors file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./colors.css";
 import { Theme, ThemePanel } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +21,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`}>
         <Theme appearance="dark">
           {children}
-          {/* <ThemePanel /> */}
+          <ThemePanel defaultOpen={false} />
         </Theme>
       </body>
     </html>
